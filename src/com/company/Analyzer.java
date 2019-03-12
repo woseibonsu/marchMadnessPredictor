2de3a4fc688d00fd84ebfa8cec749d4dd4974932
Winston O <winstonobonsu@gmail.com>
2019-03-12 17:30:15 -0400

    Introduced the Teams class and statistics list.

--- src/com/company/Analyzer.java
+++ src/com/company/Analyzer.java
@@ -1,4 +1,14 @@
 package com.company;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Analyzer {
+
+    public Analyzer()
+    {
+
+    }
+
+    
 }
--- src/com/company/Team.java
+++ src/com/company/Team.java
@@ -1,4 +1,32 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Team {
+
+    private ArrayList<Float> statList = new ArrayList<>();
+    private String teamName;
+
+    public Team(ArrayList<Float> stats) {
+
+        statList = stats;
+
+        /*
+        THE ORDER FOR STAT LIST FOLLOWS:
+
+        SEASON || TEAM ID || AVERAGE SCORE || WINS || LOSSES
+        FIELD GOALS PER GAME || FIELD GOAL SUCCESS
+        THREE POINTERS PER GAME || THREE POINTERS SUCCESS,
+        FREE THROW SUCCESS || FREE THROW SUCCESS
+        OFFENSIVE REBOUNDS PER GAME || DEFENSIVE REBOUNDS PER GAME || TURN OVERS PER GAME
+         */
+
+    }
+
+    @Override
+    public String toString()
+    {
+        return teamName;
+    }
+
 }
